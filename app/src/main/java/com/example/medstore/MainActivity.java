8810e876f438009611e0f6db261b97ad3398fc97
stha28 <prashantshrestha19@gmail.com>
2019-12-02 11:25:28 +0545

    login and Dashboard Activity

--- app/src/main/java/com/example/medstore/MainActivity.java
+++ app/src/main/java/com/example/medstore/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.medstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText etEmail, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Email, Password;
+                Email=String.valueOf(etEmail.getText());
+                Password=String.valueOf(etPassword.getText());
+                if(Email.equals("admin@abc.com") && Password.equals("admin"))
+                {
+                    Intent intent=new Intent(MainActivity.this,Dashboardactivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Username or password is incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
